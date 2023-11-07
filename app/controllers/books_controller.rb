6bdbd1f5783c5bc1726949bc879c16ca6ded78e2
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-11-07 05:14:52 +0000

    aho

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
+      flash[:success] = "Book was successfully created."
     else
       redirect_to books_path
     end
@@ -23,8 +24,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
